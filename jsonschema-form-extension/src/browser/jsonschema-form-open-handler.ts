7d6a05288e930abe7303386873ed73568c158b1e
Niklas Dahlquist <niklas.dahlquist@st.com>
2019-10-22 08:39:58 +0000

    Changed to only handle .json and default editor when name end -data

--- jsonschema-form-extension/src/browser/jsonschema-form-open-handler.ts
+++ jsonschema-form-extension/src/browser/jsonschema-form-open-handler.ts
@@ -6,7 +6,7 @@ import { EditorManager } from "@theia/editor/lib/browser";
 
 @injectable()
 export class JsonschemaFormOpenHandler extends WidgetOpenHandler<JsonschemaFormWidget> {
-    
+
     readonly id = JsonschemaFormWidget.id;
     readonly label = "Form";
 
@@ -20,11 +20,17 @@ export class JsonschemaFormOpenHandler extends WidgetOpenHandler<JsonschemaFormW
      * A returned value indicating a priority of this handler.
      */
     canHandle(uri: URI): number {
-        return this.editorManager.canHandle(uri) / 2;
+        if (uri.path.ext != ".json") {
+            return -1;
+        }
+        if (uri.path.name.endsWith("-data")) {
+            return -1;
+        }
+        return this.editorManager.canHandle(uri) * 2;
     }
 
     protected createWidgetOptions(uri: URI): JsonschemaFormWidgetOptions {
         return { uri: uri.withoutFragment().toString() };
     }
-    
+
 }
